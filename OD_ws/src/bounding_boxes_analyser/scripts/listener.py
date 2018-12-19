146aa5cc2c66b7f454b07f72b820c8e5202a7ab8
augstin9080 <augustin.laruelle@gmail.com>
2018-12-19 17:42:02 +0100

    Ecriture d'un node pour lire et convertir le topic renvoyé par YOLO.

---
+++ OD_ws/src/bounding_boxes_analyser/scripts/listener.py
@@ -0,0 +1,28 @@
+# Revision $Id$
+
+## Simple talker demo that listens to std_msgs/Strings published 
+## to the 'chatter' topic
+
+import rospy
+from darknet_ros.msg import BoudingBoxes
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
