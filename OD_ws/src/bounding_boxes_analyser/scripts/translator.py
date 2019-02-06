f93bc9c994d61e0b8919eac9361392c1b70a0e8b
auugustin <augustin9080@gmail.com>
2019-02-06 17:24:14 +0100

    Translator ok

--- OD_ws/src/bounding_boxes_analyser/scripts/translator.py
+++ OD_ws/src/bounding_boxes_analyser/scripts/translator.py
@@ -16,67 +16,68 @@ import struct
 
 FOV = math.pi/2 # field of view en radian
 
-def callback(image_yolo, prof, pub):
-	l = len(image_yolo.bounding_boxes)
-	tab = []
-	for i in range(l):
-		# camera -> robot
-		# x -> -y
-		# y -> -z
-		# z -> x
-		box = image_yolo.bounding_boxes[i]
+def callback(image_yolo,prof,pub):
+    l = len(image_yolo.bounding_boxes)
+    tab = []
+    for i in range(l):
+        # camera -> robot
+        # x -> -y
+        # y -> -z
+        # z -> x
+        box = image_yolo.bounding_boxes[i]
 
-		point1 = Point32()
-		point2 = Point32()
+        point1 = Point32()
+        point2 = Point32()
 
-		point1.z = 0
-		point2.z = 0
+        point1.z = 0
+        point2.z = 0
 
-		xcentre = (box.xmax + box.xmin)/2
-		ycentre = (box.ymax + box.ymin)/2
-		# verifier la nature de l indexation de data
-		index = 4*(ycentre * prof.width + xcentre)
-		ba = bytearray(prof.data[index : index+4])
-		depth = struct.unpack('<f', ba)[0]
-		print(depth)
-		point1.x = depth
-		point2.x = depth
+        xcentre = (box.xmax + box.xmin)/2
+        ycentre = (box.ymax + box.ymin)/2
+        # verifier la nature de l indexation de data
+        index = 4*(ycentre * prof.width + xcentre)
+        ba = bytearray(prof.data[index : index+4])
+        depth = struct.unpack('<f', ba)[0]
+        print(depth)
+        point1.x = depth
+        point2.x = depth
 
-		widthm = 2*depth*math.tan(FOV/2)
-		point1.y = widthm*(0.5-box.xmin/prof.width)
-		point2.y = widthm*(0.5-box.xmax/prof.width)
+        widthm = 2*depth    #On ne multiplie pas par tan(FOV/2) car cette quantite vaut 1 ici
+        point1.y = widthm*(0.5-box.xmin/float(prof.width))
+        point2.y = widthm*(0.5-box.xmax/float(prof.width))
 
-		poly = Polygon()
-		poly.points = [point1, point2]
+        poly = Polygon()
+        poly.points = [point1, point2]
 
-		obs = ObstacleMsg()
-		obs.header = prof.header
-		obs.polygon = poly
-		# d autres champs de l instance a completer
+        obs = ObstacleMsg()
+        obs.header = prof.header
+        obs.polygon = poly
+        # d autres champs de l instance a completer
 
-		tab.append(obs)
+        tab.append(obs)
 
-	msg = ObstacleArrayMsg()
-	msg.header = prof.header
-	msg.obstacles = tab
+    msg = ObstacleArrayMsg()
+    msg.header = prof.header
+    msg.obstacles = tab
 
-	pub.publish(msg)
-			
-	
+    pub.publish(msg)
+            
+    
 def translator():
-	rospy.init_node('translator', anonymous=True)
-	pub = rospy.Publisher('bounding_boxes_analyser/yolo_messages', ObstacleArrayMsg, queue_size=10)
-	# Publie a la reception de chaque message
-	image_yolo = message_filters.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes)
-	prof = message_filters.Subscriber('/zed/depth/depth_registered', Image)
+    rospy.init_node('translator', anonymous=True)
+    pub = rospy.Publisher('bounding_boxes_analyser/yolo_messages', ObstacleArrayMsg, queue_size=10)
+    # Publie a la reception de chaque message
+    image_yolo = message_filters.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes)
+    prof = message_filters.Subscriber('/zed/depth/depth_registered', Image)
+    
+    ts = message_filters.ApproximateTimeSynchronizer([image_yolo, prof], 10, 1, allow_headerless=True)
+                                                                # queue_size, slop (delta t en s)
+    ts.registerCallback(lambda x,y: callback(x,y,pub))
 
-	ts = message_filters.ApproximateTimeSynchronizer([image_yolo, prof], 30, 0.1, allow_headerless=True)
-	ts.registerCallback(lambda x,y: callback(x,y,pub))
-
-	# spin() simply keeps python from exiting until this node is stopped
-	rospy.spin()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
 
 
 if __name__ == '__main__':
-	translator()
+    translator()
 
