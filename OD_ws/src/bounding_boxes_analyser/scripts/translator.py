c6b7193e2c329bfa0533e0f95a5070184007360c
auugustin <augustin9080@gmail.com>
2019-02-13 16:45:21 +0100

    Visualisation ajoutée et correction du repère

--- OD_ws/src/bounding_boxes_analyser/scripts/translator.py
+++ OD_ws/src/bounding_boxes_analyser/scripts/translator.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # Revision $Id$
 
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
 
 import rospy
 from darknet_ros_msgs.msg import BoundingBoxes
@@ -13,17 +11,25 @@ import message_filters
 from sensor_msgs.msg import Image
 import math
 import struct
+from visualization_msgs.msg import Marker, MarkerArray
+
+#Reste à faire
+    # trouver une profondeur plus précise (faire plusieurs essais avec des tables)
+    # detecter les personnes pour en faire une exception avec un seuil d'acceptation
 
 FOV = math.pi/2 # field of view en radian
+repere = 'zed_left_camera_frame'
 
-def callback(image_yolo,prof,pub):
+def callback(image_yolo,prof,pub,pubRViz):
     l = len(image_yolo.bounding_boxes)
     tab = []
+    tabRViz = []
     for i in range(l):
         # camera -> robot
         # x -> -y
         # y -> -z
         # z -> x
+        # les messages sont enregistrés dans le repère de la caméra nommé: 'zed_left_camera_frame' stacké dans repere
         box = image_yolo.bounding_boxes[i]
 
         point1 = Point32()
@@ -51,8 +57,22 @@ def callback(image_yolo,prof,pub):
 
         obs = ObstacleMsg()
         obs.header = prof.header
+        obs.header.frame_id = repere
         obs.polygon = poly
         # d autres champs de l instance a completer
+        
+        #Pour la visualisation sous RViz
+        mark = Marker()
+        mark.id = i
+        mark.header = obs.header
+        mark.ns = 'obstacles'
+        mark.type = Marker.LINE_STRIP
+        mark.action = Marker.ADD
+        mark.points = poly.points
+        mark.scale.x = 0.2
+        mark.color.g = 1
+        mark.color.a = 1
+        tabRViz.append(mark)
 
         tab.append(obs)
 
@@ -61,18 +81,23 @@ def callback(image_yolo,prof,pub):
     msg.obstacles = tab
 
     pub.publish(msg)
+    
+    msgRViz = MarkerArray()
+    msgRViz.markers = tabRViz
+    pubRViz.publish(msgRViz)
             
     
 def translator():
     rospy.init_node('translator', anonymous=True)
-    pub = rospy.Publisher('bounding_boxes_analyser/yolo_messages', ObstacleArrayMsg, queue_size=10)
+    pub = rospy.Publisher('bounding_boxes_analyser/yolo_obstacles_messages', ObstacleArrayMsg, queue_size=10)
+    pubRViz = rospy.Publisher('bounding_boxes_analyser/marker_vision', MarkerArray, queue_size=10)
     # Publie a la reception de chaque message
     image_yolo = message_filters.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes)
     prof = message_filters.Subscriber('/zed/depth/depth_registered', Image)
     
     ts = message_filters.ApproximateTimeSynchronizer([image_yolo, prof], 10, 1, allow_headerless=True)
                                                                 # queue_size, slop (delta t en s)
-    ts.registerCallback(lambda x,y: callback(x,y,pub))
+    ts.registerCallback(lambda x,y: callback(x,y,pub,pubRViz))
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
